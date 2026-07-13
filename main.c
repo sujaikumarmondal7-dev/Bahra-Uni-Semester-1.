b413593ca04cdb4379685d28c971f00ca70e9251
sujaikumarmondal7-dev <sujaikumarmondaal7@gmail.com>
2026-07-13 11:04:33 +0530

    Improved ignition logic with min/max pressure checks and two-key command system

--- main.c
+++ main.c
@@ -1,27 +1,40 @@
 #include <stdio.h>
 
 int main()
-{ 
-  
-  float chamber_pressure =0.00 ;// current engine chamber pressure
-  float critical_limit_of_pressure = 4000.00 ;// maximum safe pressure limit
-
-  printf("\nmeasuring the chamber pressure:\n");
-  scanf("%f",&chamber_pressure); 
-if(chamber_pressure <=4000.00 )
 {
-  printf("\nready for engine ignition\n");
-char command ='a' ;//'s'=command for stage 1ignisation,'a'=abort/standby
-printf("\nplease enter the command:\n");
-scanf(" %c",&command);
-if(command=='s')
-{printf("\n[SYSTEM] MAIN ENGINE IGNITION SUCCESSFUL.\n");
-printf("[SYSTEM] THRUST AT 100%%. WE HAVE LIFTOFF!\n");
-    
-}}
-else
-{ printf("critical warning pressure exceeds safty limit\n");
-printf("[ACTION] engaging emergency fuel purge immediately\n");
+  //<-- start main 
+  float chamber_pressure = 0.00;
+  float critical_maximum_limit_of_pressure=5000.00;
+  float critical_minimum_limit_of_pressure=900.00;
+  //machine measuring the chamber pressure 
+  printf("\nbreading of chamber pressure :\n");
+  scanf("%f",&chamber_pressure);
+
+  if (chamber_pressure>5000.00||chamber_pressure<900.00)  
+  //here i use || because if any of the state ment is true engine ignition is risky
+ {
+  printf("\nchamber pressure is not suitable for ignition \n");
+  printf("\nit is very risky to ignite the engine\n");
 }
-return 0;
-}
+  if ( chamber_pressure<=5000.00&&chamber_pressure>=900.00)
+  { 
+    // here i add && because both the state ment above need to be true  to ignite the engine 
+    printf("chamber pressure is in suitable range \n");
+   
+    char command1 ;
+    char command2 ;
+    printf("\nplease input the commands\n");
+    scanf(" %c",&command1);
+    scanf(" %c",&command2);
+    // now i will apply condition here 
+    if(command1=='A'&&command2=='B')
+    //here i put && operator because both the input need to correct 
+    { 
+      printf("\n[SYSTEM] ALL CONDITIONS MET. LIFTOFF!\n"); 
+    }
+    else
+   {
+    printf("\n[SYSTEM] Launch aborted .check command inputs.\n");
+   }}
+   return 0;
+  }
