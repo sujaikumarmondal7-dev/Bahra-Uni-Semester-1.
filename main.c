cdaafb0b305dd8f3c9e1b7d754b2201f7c3ab609
sujaikumarmondal7-dev <sujaikumarmondaal7@gmail.com>
2026-07-18 11:00:38 +0530

    Initial commit:Added assignment files

--- main.c
+++ main.c
@@ -1,33 +1,26 @@
 #include <stdio.h>
-#include <stdlib.h> 
-#include <time.h>
 #include <ctype.h>
+#include <time.h>
+#include <stdlib.h>
 int main()
-{
-int random_number;
-char input_character;
-
-// now we will set the condition
-    srand(time(NULL));
-    random_number=(rand() % 10)+1;
-   printf("\nplease input the input_character:\n");
-   scanf(" %c",&input_character); 
-   if( isdigit(input_character))
    {
-    //fixed:added  the math conversion line here!
-    int input_number=input_character-'0';
-    //now compare the converted integer now 
-   
-    if(random_number==input_number)
+    int die1;
+    int die2;
+    int totalsum;
+    //now we will add the process of generating the random outcome
+    printf("\nplease ROLL the dies\n");
+    srand(time(NULL));
+    die1=(rand()%6)+1;
+    die2=(rand()%6)+1;
+    //now it is the time for total sum 
+    totalsum=die1+die2;
+    if((totalsum==7)||(totalsum==11))
     {
-        printf("\nyou have gussed the correct input\n");}
-        else
-        {printf("\nyou gussed the wrong number\n");}
-    
+        printf("\nplayer win\n");
     }
     else
     {
-        printf("\ncharacters are not allowed\n");
+        printf("\nthe sum of the two dice is %d . Thank you for playing !\n",totalsum);
     }
     return 0;
-}
+   }
--- submarine.c
+++ submarine.c
@@ -1,47 +1,48 @@
 #include <stdio.h>
 #include <ctype.h>
-int main(){
-   float water_pressure=0.0;
-   float critical_maximum_water_pressure=150.0;
-   // if pressure is 150.0 submarine should not go below that level of see 
-   float critical_minimum_water_pressure=1.0;
-   // it represent that pressure measuring device is giving a false reading 
-   //which is not possible inside the sea 
-   printf("\nreading of water_pressure\n");
-   scanf("%f",&water_pressure) ;
-   if(water_pressure<1.0)
-   { 
-    printf("\n[CRITICAL] a problem is dected in pressure reading device\n");
-    printf("\n it is not safe to open the hull\n");
+int main()
+    
+{ 
+    float moving_speed;//here we are talking about the speed of train
+    // by using the speed sensor device,
+    printf("\ndected speed:\n");
+    scanf("%f",&moving_speed);
+      //now we will apply conditions 
+      if(moving_speed<0.0)// which is not possible 
+      {
+        printf("\n[CRITICAL]speed sensor error dected\n");
+        printf("\nfor safty precaution gate remain closed\n");
+      } 
+   //also
+   else if(moving_speed>5.0)
+   { printf("\n[WARNING]train speed must come down below 5.0 kmph \n");
+    printf("\nfor safty precation door will remain closed\n");
 
-    }
-    else if (water_pressure>150.0)
-    {
-        printf("[CRITICAL] Pressure exceeds hull structural limits!");
-        printf("\n it is not safe to open the hull\n");
-     }
-
-    if (water_pressure>=1.0&&water_pressure<=150.0)
-    {
-        printf("\n[SYSTEM] External pressure stable. Safe to proceed.\n");
-        char command1;
-        char command2;
-        // now it is time to enter the command 
-        printf("\nplease input the command\n");
-        scanf(" %c",&command1);
-        scanf(" %c",&command2);
-       //using a simple if .(it is odd but i will change it latter)
-       if(isdigit(command1)||isdigit(command2))
-       {
-        printf("\n[error] system malfunction:digits are not accepted as security keys");
-            printf("\n[denied]hatch remains locked\n");
-       }
-       //now i will apply condition 
-       else if (command1=='X'&&command2=='Y')
-        {printf("[SUCCESS] Hatch unlocked. Welcome aboard, Captain!"); }
-else
-{printf("[DENIED] Security violation. Hatch remains locked");} 
+   }
+   if(moving_speed>=0.0&&moving_speed<=5.0)//safly docking speed
+   {
+   printf("\ntrain safely docked at the platform\n");
+   // time to board the train 
+   
+   char input1;
+   char input2;
+   printf("\nplease input the platform authorization code\n");
+   scanf(" %c",&input1);
+   scanf(" %c",&input2);
+   // now we will set the condition
+   if (isdigit(input1)||isdigit(input2))
+   printf("\ndigits are not allowed.please verify your code\n ");
+ else if(input1=='A'&&input2=='B') 
+ {printf("\n[SUCCESS] platform gates open.welcoming passenger\n");}
+ else{ 
+    printf("\naccess denied\n");
  }
-return 0;
+} 
+   return 0;
+   }
 
-}
+   
+   
+   
+   
+    
